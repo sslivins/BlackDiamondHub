f62ea8f061f5d0977e57db7ca089f6009b5509bc
Stefan Slivinski <sslivins@gmail.com>
2024-08-27 20:36:23 -0700

    scraping of sun peaks weather now working

--- BlackDiamondHub/views.py
+++ BlackDiamondHub/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+from django.shortcuts import render
 
 def landing_page(request):
-    return render(request, 'index.html')
+    print("Fetching external content...")
+    url = 'https://www.sunpeaksresort.com/bike-hike/weather-webcams/weather'  # Replace with the URL you want to fetch
+    response = requests.get(url)
+    
+    print(f"Status code: {response.status_code}")
+
+    # Parse the HTML content
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Extract specific content
+    content = soup.find('div', {'class': 'weather current-conditions'})  # Adjust as needed
+    
+    print(f"Content: {content}")
+
+    # Pass the content to the template
+    context = {
+        'external_content': str(content)
+    }
+    return render(request, 'index.html', context)
