a0bd0379725edc8c1c1e53cdc01d97a730593a94
Stefan Slivinski <sslivins@gmail.com>
2024-08-30 15:51:58 -0700

    added delete and bulk action buttons

--- feedback/urls.py
+++ feedback/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('submit-feedback/', views.submit_feedback, name='submit_feedback'),
-    path('feedback/', views.view_feedback, name='view_feedback'),
-    path('feedback/mark_as_read/<int:feedback_id>/', views.mark_as_read, name='mark_as_read'),    
+    path('', views.view_feedback, name='view_feedback'),
+    path('mark_as_read/<int:feedback_id>/', views.mark_as_read, name='mark_as_read'),
+    path('delete/<int:feedback_id>/', views.delete_feedback, name='delete_feedback'),
+    path('bulk-action/', views.bulk_feedback_action, name='bulk_feedback_action'),    
 ]
