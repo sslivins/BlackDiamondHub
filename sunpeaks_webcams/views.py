9c32be16ecbd8fab61490366e81a5634f91d4a1d
Stefan Slivinski <sslivins@gmail.com>
2024-08-29 19:50:54 -0700

    update images in place anytime they change

--- sunpeaks_webcams/views.py
+++ sunpeaks_webcams/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
-from django.shortcuts import render
+from urllib.parse import urlsplit, urlunsplit, parse_qs
 
 # Create your views here.
 def webcams(request):
     url = 'https://www.sunpeaksresort.com/bike-hike/weather-webcams/webcams'  # Replace with the URL you want to fetch
     domain = 'https://www.sunpeaksresort.com'
-    modal_script = 'openModal(this.src, this.alt)'
     response = requests.get(url)
     
     # Parse the HTML content
@@ -16,6 +15,9 @@ def webcams(request):
     # Extract specific content
     content = soup.find('div', {'id': 'webcams'})
     
+    # Collect image timestamps
+    image_timestamps = {}    
+  
     if content:
         # Iterate over all img tags within the content
         for img_tag in content.find_all('img'):
@@ -23,10 +25,15 @@ def webcams(request):
             if img_tag['src'].startswith('/'):
                 img_tag['src'] = domain + img_tag['src']
                 
-            # Add the modal script to the img tag
-            img_tag['onclick'] = modal_script
-    
+            if 'timestamp=' in img_tag['src']:
+              split_url = urlsplit(img_tag['src'])
+              base_url = urlunsplit((split_url.scheme, split_url.netloc, split_url.path, '', ''))
+              query_params = parse_qs(split_url.query)
+              timestamp = query_params.get("timestamp", [None])[0]
+              image_timestamps[base_url] = timestamp
+                
     context = {
-        'external_webcams': str(content)
+        'external_webcams': str(content),
+        'image_timestamps': image_timestamps,
     }
     return render(request, 'webcams.html', context)
