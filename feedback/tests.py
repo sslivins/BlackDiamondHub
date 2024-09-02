2b13b12c2238646a79590862cf6407918ecfbe30
Stefan Slivinski <sslivins@gmail.com>
2024-09-02 09:40:17 -0700

    all tests passing

--- feedback/tests.py
+++ feedback/tests.py
@@ -25,15 +25,39 @@ class FeedbackViewTests(TestCase):
 
     def test_pagination(self):
         """Test that pagination works and shows correct number of items per page."""
+        
+        """remove any existing messages"""
+        Feedback.objects.all().delete()
+        response = self.client.get(reverse('view_feedback'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No messages")        
+        
         # Create additional feedback messages
         for i in range(30):
             Feedback.objects.create(name=f'User{i+3}', email=f'user{i+3}@example.com', message=f'Test message {i+3}', is_read=False)
 
         response = self.client.get(reverse('view_feedback'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Page 1 of 2")
-        self.assertContains(response, "User1")
-        self.assertNotContains(response, "User21")
+
+        # Verify that 20 items are on the first page
+        table = response.context['table']
+        self.assertEqual(len(table.page), 20)
+        
+        # Check if the pagination information is correct
+        paginator = table.paginator
+        self.assertEqual(paginator.num_pages, 2)  # There should be 2 pages
+        self.assertEqual(table.page.number, 1)  # We should be on the first page
+
+        # Now request the second page
+        response = self.client.get(reverse('view_feedback') + '?page=2')
+        self.assertEqual(response.status_code, 200)
+        
+        table = response.context['table']
+        self.assertEqual(len(table.page), 10)  # There should be 10 items on the second page
+        
+        paginator = table.paginator
+        self.assertEqual(paginator.num_pages, 2)  # Still 2 pages
+        self.assertEqual(table.page.number, 2)  # Now we should be on the second page
 
     def test_bulk_delete(self):
         """Test bulk delete action."""
@@ -55,6 +79,17 @@ class FeedbackViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.feedback1.refresh_from_db()
         self.assertTrue(self.feedback1.is_read)
+        
+    def test_mark_unread(self):
+        """Test bulk mark as unread action."""
+        response = self.client.post(reverse('bulk_feedback_action'), {
+            'feedback_ids': [self.feedback2.id],
+            'action': 'mark_unread'
+        }, content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.feedback2.refresh_from_db()
+        self.assertFalse(self.feedback2.is_read)
 
     def test_modal_data(self):
         """Test that modal opens with correct feedback data."""
