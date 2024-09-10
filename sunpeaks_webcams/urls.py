5d10d678583e35e4c7021107b5db8c550a2efb03
Stefan Slivinski <sslivins@gmail.com>
2024-09-09 19:50:41 -0700

    switched to list of webcams rather than html

--- sunpeaks_webcams/urls.py
+++ sunpeaks_webcams/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.webcams, name='webcams'),
+    path('get_webcams/', views.check_for_new_webcams_json, name='check_for_new_webcams_json'),
 ]
