4218bc3699e80d955f8761a0ca6febde2042ab9a
sslivins <sslivins@users.noreply.github.com>
2024-09-10 20:06:36 -0700

    Webcam_improvements (#10)
    * switched to list of webcams rather than html
    
    * table clean up
    
    * improved styling and sizing on connect monitor
    
    * added feedback modal tests on webcam page
    
    * added border to webcam image
    
    * switched to v4 upload artifact

--- sunpeaks_webcams/views.py
+++ sunpeaks_webcams/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urlsplit, urlunsplit, parse_qs
+from urllib.parse import urlsplit, parse_qs
+from django.http import JsonResponse
 
 # Create your views here.
 def webcams(request):
+
+    # Check for new webcams
+    webcams = check_for_new_webcams()
+    
+    context = {
+        'sunpeaks_webcams': webcams
+    }
+    
+    return render(request, 'webcams.html', context)
+
+def check_for_new_webcams_json(request):
+
+    webcams_list = check_for_new_webcams()  # Get the webcam data
+    return JsonResponse(webcams_list, safe=False)
+
+def check_for_new_webcams():
+
     url = 'https://www.sunpeaksresort.com/bike-hike/weather-webcams/webcams'  # Replace with the URL you want to fetch
     domain = 'https://www.sunpeaksresort.com'
     response = requests.get(url)
@@ -15,25 +33,42 @@ def webcams(request):
     # Extract specific content
     content = soup.find('div', {'id': 'webcams'})
     
-    # Collect image timestamps
-    image_timestamps = {}    
-  
     if content:
-        # Iterate over all img tags within the content
-        for img_tag in content.find_all('img'):
-            # Prepend the domain to relative image paths
-            if img_tag['src'].startswith('/'):
-                img_tag['src'] = domain + img_tag['src']
+        cameras = soup.find_all('div', class_='cam')
+        webcams_list = []
+              
+        for cam in cameras:
+            camera_name = cam.find('h2').text.strip()
+
+            # Extract image URL from the <img> tag
+            image_url = cam.find('div', class_='image').find('img')['src']
+            if image_url.startswith('/'):
+                image_url = domain + image_url
                 
-            if 'timestamp=' in img_tag['src']:
-              split_url = urlsplit(img_tag['src'])
-              base_url = urlunsplit((split_url.scheme, split_url.netloc, split_url.path, '', ''))
+            if 'timestamp=' in image_url:
+              split_url = urlsplit(image_url)
               query_params = parse_qs(split_url.query)
               timestamp = query_params.get("timestamp", [None])[0]
-              image_timestamps[base_url] = timestamp
+
+            # Extract table rows to get last_updated, location, and elevation
+            table_rows = cam.find('table').find_all('tr')
+            last_updated = table_rows[0].find_all('td')[1].text.strip()
+            location = table_rows[1].find_all('td')[1].text.strip()
+            elevation = table_rows[2].find_all('td')[1].text.strip()
+
+            # Create a structured dictionary for each camera
+            webcam_info = {
+                'camera_name': camera_name,
+                'image_url': image_url,
+                'timestamp': timestamp,
+                'last_updated': last_updated,
+                'location': location,
+                'elevation': elevation
+            }
+            
+            # Append to the list of camera data
+            webcams_list.append(webcam_info)              
                 
-    context = {
-        'external_webcams': str(content),
-        'image_timestamps': image_timestamps,
-    }
-    return render(request, 'webcams.html', context)
+    return webcams_list
+    
+    
