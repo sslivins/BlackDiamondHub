ff94c13684a897a6ed58cf7e43d4d29cd2debbf2
Stefan Slivinski <sslivins@gmail.com>
2024-09-19 08:58:35 -0700

    only send speaker update when something changes

--- sonos_control/consumers.py
+++ sonos_control/consumers.py
@@ -7,7 +7,6 @@ import asyncio
 
 class SonosConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        print('WebSocket connected')
         await self.accept()
         self.speaker_group_name = 'sonos_speakers'
 
@@ -17,8 +16,12 @@ class SonosConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
-        # Start the task to send updates periodically
-        self.send_task = asyncio.create_task(self.send_speaker_updates())
+        # Initialize the previous state to None
+        self.previous_speaker_data = None
+
+        # Start the task to send updates periodically, only if not already running
+        if not hasattr(self, 'send_task') or self.send_task.done():
+            self.send_task = asyncio.create_task(self.send_speaker_updates())
 
     async def disconnect(self, close_code):
         # Leave the group
@@ -27,8 +30,12 @@ class SonosConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
-        # Cancel the periodic task
-        self.send_task.cancel()
+        # Cancel the periodic task if it exists
+        if hasattr(self, 'send_task') and not self.send_task.done():
+            try:
+                self.send_task.cancel()
+            except asyncio.CancelledError:
+                pass
 
     async def receive(self, text_data):
         # You can handle messages from the client here if needed
@@ -39,7 +46,7 @@ class SonosConsumer(AsyncWebsocketConsumer):
             # Fetch the latest speaker info
             speakers_info = get_sonos_speaker_info()
 
-            # Prepare data to send
+            # Prepare the new data to send
             speaker_data = {}
             for info in speakers_info:
                 if info['is_coordinator']:
@@ -53,11 +60,15 @@ class SonosConsumer(AsyncWebsocketConsumer):
                         'queue': info['queue']
                     }
 
-            # Send the data to the WebSocket
-            await self.send(text_data=json.dumps({
-                'type': 'speaker_update',
-                'speaker_data': speaker_data
-            }))
+            # Check if the data has changed
+            if speaker_data != self.previous_speaker_data:
+                # Data has changed, send it to the WebSocket
+                await self.send(text_data=json.dumps({
+                    'type': 'speaker_update',
+                    'speaker_data': speaker_data
+                }))
+                # Update the previous state
+                self.previous_speaker_data = speaker_data
 
-            # Wait for 5 seconds before sending the next update
-            await asyncio.sleep(5)
+            # Wait for 5 seconds before checking again
+            await asyncio.sleep(3)
