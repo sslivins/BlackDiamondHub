1b7ee416ab308b363286a55fcc8ec8f731298a8d
Stefan Slivinski <stesli@microsoft.com>
2025-02-12 11:04:24 -0800

    added readme and fixed a few settings

--- BlackDiamondHub/settings.py
+++ BlackDiamondHub/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-*-@iek83wyv#jk^*h_wnxj93&hn#%==cqoyfey17r_b6jy43$r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.162"]
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
 
 
 # Application definition
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'BlackDiamondHub.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sunpeaks_inventory',
+        'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
