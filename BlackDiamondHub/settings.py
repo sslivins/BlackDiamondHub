791dff900a8468619a492ea4fabed4c3eceb5084
sslivins <sslivins@users.noreply.github.com>
2025-02-14 09:37:05 -0800

    Add website icon (#16)
    * added website icons
    
    * enabled allow host environment variable to support a list of hosts
    
    * fixed misspelled method
    
    * added additional icons

--- BlackDiamondHub/settings.py
+++ BlackDiamondHub/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = 'django-insecure-*-@iek83wyv#jk^*h_wnxj93&hn#%==cqoyfey17r_b6jy43$r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',') if os.getenv('ALLOWED_HOSTS') else []
 
 # Application definition
 
